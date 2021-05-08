051a3b69e69c9f5195e2f5f8ab92d8d3f8cdd27e
Bill Gan <billgan12345@gmail.com>
2021-05-08 12:00:33 -0400

    second commit

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const path = require("path");
 
 const app = express();
 
-//use heroku's port to use an available one
-//this port for the express server (the thing that interacts with react's frontend) 
-//it is different from the mongodb server url/port
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -17,8 +14,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test", {
     useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
 });
 
-//event: on connect open (only listens once)
-//this will now load up all the data on the current server to be used in the front end
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
